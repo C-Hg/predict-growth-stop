748e7e1ce428d5c2f76d6949e84de45151a2f7f7
Camille Hagenbourger <camille.hagenbourger@protonmail.ch>
2020-10-01 20:08:28 +0200

    commit app build

---
+++ webapp/build/precache-manifest.0b11e67a18132a964ab27f8a9583c910.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a47c8ffcaf9c16e6120c69a3d7bed2ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f96e043a3c42ebbc0aeb",
+    "url": "/static/js/2.22c1874a.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.22c1874a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b687a4fb5d8eecc0c9",
+    "url": "/static/js/main.35539ff3.chunk.js"
+  },
+  {
+    "revision": "b3cfd9f5cdf691e24e2f",
+    "url": "/static/js/runtime-main.00ed32f5.js"
+  }
+]);
